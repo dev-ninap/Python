4715d766f384eb9fd52abc2871c057abcff3c817
dev-ninap <ninalisapereira@gmail.com>
2025-09-17 22:55:56 -0300

    Update exerc.py

--- exerc.py
+++ exerc.py
@@ -1,3 +1,6 @@
 pessoa = {"nome": "Ana Luiza", "idade":20, "cidade": "Barra mansa"}
 pessoa.update({"profissão": "Nutricionista"})
 print(pessoa["nome"], pessoa["profissão"])
+
+pessoa = {"nome": "Nina", "idade":20. "cidade": "Barra mansa"}
+pessoa.update
